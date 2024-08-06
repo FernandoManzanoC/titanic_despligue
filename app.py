53415f19432563350b9ad194a1094edec52d51d6
FernandoManzanoC <158820850+FernandoManzanoC@users.noreply.github.com>
2024-08-06 18:55:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def retrain():  # Rutarlo al endpoint '/api/v1/retrain', metodo POST
         y = data[target]
 
         # Dividir el dataset en entrenamiento y prueba
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=24)
 
         # Entrenar un nuevo modelo
         model = RandomForestClassifier(n_estimators=100, random_state=42)
