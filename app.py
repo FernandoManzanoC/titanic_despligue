56e6fe3b3dec1d62a416178b4a55cad2ce4b40ab
FernandoManzanoC <158820850+FernandoManzanoC@users.noreply.github.com>
2024-08-07 16:27:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def retrain():  # Rutarlo al endpoint '/api/v1/retrain', metodo POST
         X = data[features]
         y = data[target]
 
-        # Dividir el dataset en entrenamiento y prueba
+        # Dividir el dataset en entrenamiento y prueba. Cambiado random a 24 desde 42
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=24)
 
         # Entrenar un nuevo modelo
